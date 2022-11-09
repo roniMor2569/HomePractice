fe3eb53552f1113aece2c2aa6ba54d1a9298785c
Roni Mor <ronimor@h-MBP-sl-Roni.lan>
2022-11-09 20:14:08 +0200

    create first layout

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const productContainer = document.getElementById("products-container");
 
+const product = {
+    name: 'Black Tea';
+    amaount: 33, 
+    price: 20
+}
+
+//create main element
+const productDiv = document.createElement("div"); 
+productDiv.classList("product");
+
+//create name container
+const productNameDiv = document.createElement("div");
+productNameDiv.innerHTML = product.name;
+productDiv.appendChild(productNameDiv);
+
+//create amount container
+const productAmountDiv = document.createElement("div");
+productAmountDiv.innerHTML = product.amaount;
+product.appendChild(productAmountDiv);
+
+
+
+
+
+
+
+
 const productList = ["Black Tea", "coffe", "Green Tea", "milk"];
 
 for (let i=0; i<productList.length; i++) {
